845a190c511c3d36ee8dfa226a8770ce00e6a8b4
Petter Astrup <petter.astrup@gmail.com>
2015-05-09 13:58:46 -0700

    .gitignore is now working

--- config.py
+++
@@ -1,8 +0,0 @@
-WTF_CSRF_ENABLED = False
-SECRET_KEY = 'mysecretkeyissecret'
-
-# MongoDB settings for mongoengine
-MONGODB_SETTINGS = {
-    "db": "Vendors",
-    "host": "mongodb://testuser:test@ds031812.mongolab.com:31812/ineedvendors"
-}
