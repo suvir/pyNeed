dabd244e4761edfabcf8c53779b17fbc50ea15b1
Anjali Kanak <akanak@eng.ucsd.edu>
2015-05-30 11:21:41 -0700

    Fixed remove deal code and added workaround for create deal

--- app/deals_utilities.py
+++ app/deals_utilities.py
@@ -1,70 +1,66 @@
 import requests
 import json
-from models import Vendor, Deal, Product
-#from db_utilities import *
 import db_utilities as dbutil
 
 product_url = 'https://ineed-db.mybluemix.net/api/items'
 createDeal_url = 'http://ineed-dealqq.mybluemix.net/createDeal'
 deleteDeal_url = 'http://ineed-dealqq.mybluemix.net/deleteDeal'
 getDeal_url = 'http://ineed-dealqq.mybluemix.net/getOneDeal'
-deal_url = 'https://ineed-db.mybluemix.net/api/deals'
+findDeal_url = 'http://ineed-dealqq.mybluemix.net/findDeal'
+
 id_field = '_id'
 
 
 def deals_to_json(deal, vendorName, vendorId):
     deal_json = {}
-    deal_json['dealName'] = deal.name
-    deal_json['vendorName'] = vendorName
-    deal_json['vendorId'] = vendorId
+    deal_json['deal_name'] = deal.name
+    deal_json['vendor_name'] = vendorName
+    deal_json['vendorID'] = vendorId
     deal_json['type'] = deal.category
     deal_json['price'] = str(deal.price)
     deal_json['discount'] = str(deal.discount)
-    deal_json['expireDate'] = deal.expiry_date
-    deal_json['couponCode'] = deal.description
+    deal_json['expire'] = deal.expiry_date
+    deal_json['coupon_code'] = deal.description
 
     products = []
     prod_id, product = dbutil.get_single_product(vendorId, deal.product_name)
     products.append(str(prod_id))
-    deal_json['itemSell'] = products
-
-    print "********************************************"
-    print json.dumps(deal_json)
-    print "********************************************"
+    deal_json['item_sell'] = json.dumps(products)
 
     return deal_json
 
 
 def get_single_deal(vendorId, dealName):
-    params = {"vendorId": vendorId, "dealName": dealName}
+    params = {"vendorId": vendorId}
     r = requests.get(findDeal_url, params=params)
     deals = r.json()
-
     if len(deals) == 0 or deals is None:
-        print "No deals with vendorId found"
-
-    deal = deals[0]
-    deal_id = deal[id_field]
-    return deal_id, deal
+        print "No deals named ", dealName, " found"
+    for item in deals:
+        if item['dealName'] == dealName:
+            return item[id_field], item
 
 
 def post_single_deal(vendorId, deal, vendorName):
     deal_json = deals_to_json(deal, vendorName, vendorId)
     for k,v in deal_json.items():
         print k, type(v),v
-    r = requests.post(deal_url, data=deal_json)
+    r = requests.post(createDeal_url, data=deal_json)
     print "Posting single deal", r
     return r
 
 
 def delete_single_deal(vendorId, deal, vendorName):
-    params = {"vendorId": vendorId, "dealName": deal.name}
-    r = requests.get(getDeal_url, params=params)
+    params = {"vendorId": vendorId}
+    r = requests.get(findDeal_url, params=params)
     try:
         deals = r.json()
-        deal_id = deals[0][id_field]
-        r = requests.delete(deleteDeal_url + '/' + deal_id)
-        print "Deleting single deal", r
+        for item in deals:
+            if item['dealName'] == deal.name:
+                deal_id = item[id_field]
+                params = {"deal_id": deal_id}
+                r = requests.delete(deleteDeal_url, params=params)
+                print "Deleting single deal", r
     except:
         print "No such deals found in database"
 
